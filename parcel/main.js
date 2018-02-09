e1da39f5660008d15a969e8ac9bce60e93142f95
Miró Sorja <ping@mirosorja.com>
2018-02-09 13:55:01 +0200

    Show files

---
+++ parcel/main.js
@@ -0,0 +1,6 @@
+// import {add} from './rust/add.rs'
+// import {fibonacci, fibonacci_recursive} from './rust/fibonacci.rs'
+import {fib} from './wasm/fibonacci.wasm'
+console.log(fib(40))
+// console.log(fibonac
+
